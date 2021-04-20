2916454815303d37395646074b77db6f2950a1fd
Yizhuo Ren <ren.yiz@husky.neu.edu>
2021-04-19 22:29:17 -0700

    assignment8

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(res => console.log("Connected to DB"))
+.catch(err => console.log(err))
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())

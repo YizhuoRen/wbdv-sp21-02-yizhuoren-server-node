2a62908386c243e5a3671756b6e3d246dee34916
Yizhuo Ren <ren.yiz@husky.neu.edu>
2021-04-19 22:11:02 -0700

    assignment8

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/whiteboard-02', {useNewUrlParser: true, useUnifiedTopology: true});
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
